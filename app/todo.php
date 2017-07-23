13772204e48df237404d5f00c720d6702870f43e
keyrwinz <keyrwinfelisilda@gmail.com>
2017-07-23 22:48:36 +0800

    using mutator

--- app/todo.php
+++ app/todo.php
@@ -6,8 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class todo extends Model
 {
+	/*
     public function getBodyAttribute($value)
     {
     	return ucfirst($value);
+    }
+    */
+   
+    public function setBodyAttribute($value)
+    {
+    	return $this->attributes['body'] = ucfirst($value);
     }
 }
